7496122f02ab0551446608f878bc7c03bd390ba1
Richard Davis <rdavis.bacs@gmail.com>
2014-12-10 11:37:12 -0500

    Add test for uniqueness on email

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -9,7 +9,7 @@ feature "Input a Board Game", %q(
 
   - [x] There is a link to 'Sign Up' on the homepage.
   - [x] If I fill in my first name, last name, email, password, and password confirmation correctly, I am greeted with a confirmation message that my account has been created.
-  - [ ] If the password and password confirmation fields do not match, I am given an error message.
+  - [x] If the password and password confirmation fields do not match, I am given an error message.
   - [ ] If my email already exists in the database, I am given a message that tells me I have already registered.
   - [ ] If my email is not formatted correctly, I am given an error message.
 ) do
@@ -40,5 +40,26 @@ feature "Input a Board Game", %q(
     click_on "Sign up"
 
     expect(page).to have_content "Password confirmation doesn't match"
+  end
+
+  scenario "email is already registered" do
+    existing_user = User.create(
+      first_name: "Gene",
+      last_name: "Parmesan",
+      email: "gparmesan@privateeyes.com",
+      password: "supersecret"
+    )
+
+    visit root_path
+    click_on "Sign Up"
+
+    fill_in "First Name", with: existing_user.first_name
+    fill_in "Last Name", with: existing_user.last_name
+    fill_in "Email", with: existing_user.email
+    fill_in "Password", with: existing_user.password
+    fill_in "Password confirmation", with: existing_user.password
+    click_on "Sign up"
+
+    expect(page).to have_content "Email has already been taken"
   end
 end
